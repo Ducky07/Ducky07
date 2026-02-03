9929c79413cfbb7c96c15c9e08f9c76266debaef
Justin Valley <jack.valley@outlook.com>
2026-02-03 21:39:49 +0200

    script update

--- update_configs.sh
+++ update_configs.sh
@@ -2,15 +2,32 @@
 
 set -e
 
-sudo pacman -S ghostty
-sudo pacman -S fish
-sudo pacman -S starship
+packages=("ghostty" "fish" "starship")
+
+missing=()
+
+for pkg in "${packages[@]}"; do
+    if ! pacman -Qi "$pkg" > /dev/null 2>&1; then
+        missing+=("$pkg")
+    fi
+done
+
+if [ ${#missing[@]} -eq 0 ]; then
+    echo "Everything is already installed"
+else
+    echo "Installing: ${missing[*]}"
+    sudo pacman -S "${missing[@]}"
+fi
+
+ghostty &
+sleep 2
 
 #Ghostty - 0xProto font recommended
-cp /files/config ~/.config/ghostty/config
+cp ./files/config ~/.config/ghostty/config
 #Fish
-cp /files/config.fish ~/.config/fish/config.fish
+cp ./files/config.fish ~/.config/fish/config.fish
 #Starship
-cp /files/starship.toml ~/.config/starship.toml
+cp ./files/starship.toml ~/.config/starship.toml
 
+clear
 echo "Configuration installation and update complete"
